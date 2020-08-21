3dd2603f6b06bfba299841cdc9d8b835994989f3
rohitnairtech <puthenrohit@gmail.com>
2020-08-21 16:41:47 +0530

    Created graphs with two sample images

---
+++ image.py
@@ -0,0 +1,38 @@
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+img = Image.open('benford.jpg').convert("L")
+arr = np.array(img)
+
+print(arr)
+
+
+
+shape = arr.shape
+print(shape)
+numSeries = {'1':0, '2':0, '3':0, '4':0, '5':0, '6':0, '7':0, '8':0, '9':0}
+
+flat_arr = arr.ravel()
+print(flat_arr)
+totalLen = len(flat_arr)
+for num in  flat_arr:
+    firstDigit = str(int(num))[:1]
+    if(firstDigit != '0'):
+        numSeries[firstDigit] += 1 
+
+x_list, y_list = [], []
+for key, num in numSeries.items():
+    x_list.append(key)
+    y_list.append(num)
+print(x_list)
+print(y_list)
+
+plt.plot(x_list, y_list)
+
+plt.xlabel('x - axis')
+plt.ylabel('y - axis')
+
+plt.title('Benfords graph!')
+
+plt.show()
